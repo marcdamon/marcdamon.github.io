847934f0aa4175602ec63e1c3baec66d801f75c1
marcdamon <mdamon@me.com>
2023-03-18 20:46:53 -0500

    f

--- script.js
+++ script.js
@@ -102,19 +102,22 @@ document.getElementById("next-question").addEventListener("click", () => {
 /*-----------------------------------------------------------
 Study Guide home page
 */
-document.getElementById("review-questions-answers").addEventListener("click", () => {
-    // Implement the functionality for reviewing all questions with answers shown
-});
-
-document.getElementById("review-questions").addEventListener("click", () => {
-    window.location.href = "questions-without-answers.html";
-});
-
-document.getElementById("review-uncertain-questions").addEventListener("click", () => {
-    // Implement the functionality for reviewing uncertain questions only
-});
-
-const randomToggle = document.getElementById("random-toggle");
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("review-questions-answers").addEventListener("click", () => {
+        // Implement the functionality for reviewing all questions with answers shown
+    });
+  
+    document.getElementById("review-questions").addEventListener("click", () => {
+        window.location.href = "questions-without-answers.html";
+    });
+  
+    document.getElementById("review-uncertain-questions").addEventListener("click", () => {
+        // Implement the functionality for reviewing uncertain questions only
+    });
+  
+    const randomToggle = document.getElementById("random-toggle");
+  });
+  
 
   
   function generateRandomOrder(length) {
