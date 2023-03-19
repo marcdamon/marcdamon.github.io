2900111f4822aa901c69574ea0c0ef202bfac454
marcdamon <mdamon@me.com>
2023-03-18 20:52:02 -0500

    f

--- study-guide.js
+++ study-guide.js
@@ -11,3 +11,25 @@ document.getElementById("review-uncertain-questions").addEventListener("click", 
 });
 
 const randomToggle = document.getElementById("random-toggle");
+
+function generateRandomOrder(length) {
+    const indices = Array.from({ length }, (_, i) => i);
+    for (let i = indices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [indices[i], indices[j]] = [indices[j], indices[i]];
+    }
+    return indices;
+  }
+  
+  randomToggle.addEventListener("change", () => {
+    if (randomToggle.checked) {
+      // Show questions in random order
+      const randomOrder = generateRandomOrder(questions.length);
+      const randomQuestions = randomOrder.map(index => questions[index]);
+      // Replace 'questions' variable with 'randomQuestions'
+    } else {
+      // Show questions in order
+      // Use the original 'questions' variable
+    }
+  });
+  
