78bbae5ab614206b99bd97a02e8ad4dc68bdadc4
marcdamon <mdamon@me.com>
2023-03-18 20:49:29 -0500

    s

---
+++ study-guide.js
@@ -0,0 +1,13 @@
+document.getElementById("review-questions-answers").addEventListener("click", () => {
+    // Implement the functionality for reviewing all questions with answers shown
+});
+
+document.getElementById("review-questions").addEventListener("click", () => {
+    window.location.href = "questions-without-answers.html";
+});
+
+document.getElementById("review-uncertain-questions").addEventListener("click", () => {
+    // Implement the functionality for reviewing uncertain questions only
+});
+
+const randomToggle = document.getElementById("random-toggle");
