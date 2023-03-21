8f18e0f248c52982b516329cbdd8c7cd0caea6d0
marcdamon <mdamon@me.com>
2023-03-21 10:06:56 -0500

    d

--- script-without-answers.js
+++ script-without-answers.js
@@ -1,4 +1,13 @@
+const randomOrder = localStorage.getItem("randomOrder") === "true";
 
+function generateRandomOrder(length) {
+  const indices = Array.from({ length }, (_, i) => i);
+  for (let i = indices.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [indices[i], indices[j]] = [indices[j], indices[i]];
+  }
+  return indices;
+}
   
 
 
