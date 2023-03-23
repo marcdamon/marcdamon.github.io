9aa67b22353b8fc99d3a1d21be8b24e021c42662
marcdamon <mdamon@me.com>
2023-03-23 17:43:46 -0500

    d

--- script-without-answers.js
+++ script-without-answers.js
@@ -1,76 +1,84 @@
-
-
-
 import { orderedQuestions } from "./script-questions.js";
 
+function saveMarkedQuestions() {
+  localStorage.setItem('markedQuestions', JSON.stringify(Array.from(markedQuestions)));
+}
 
-
-  let currentQuestionIndex = 0;
-  
-  const questionElement = document.getElementById("question");
-  const questionLabelElement = document.getElementById("question-label");
-  const answerElement = document.getElementById("answer");
-  
-  function showQuestion() {
-    questionElement.textContent = orderedQuestions[currentQuestionIndex].question;
-    answerElement.textContent = orderedQuestions[currentQuestionIndex].answer;
-    answerElement.hidden = true;
-    questionLabelElement.textContent = `Question ${currentQuestionIndex + 1} of ${orderedQuestions.length}`;
-    updateMarkForReviewBtn();
+function loadMarkedQuestions() {
+  const loadedMarkedQuestions = localStorage.getItem('markedQuestions');
+  if (loadedMarkedQuestions) {
+    const parsedMarkedQuestions = JSON.parse(loadedMarkedQuestions);
+    parsedMarkedQuestions.forEach((questionIndex) => {
+      markedQuestions.add(questionIndex);
+    });
   }
-  
-  document.getElementById("show-answer").addEventListener("click", () => {
-    answerElement.hidden = !answerElement.hidden;
-    if (answerElement.hidden) {
-      document.getElementById("show-answer").textContent = "Show Answer";
-    } else {
-      document.getElementById("show-answer").textContent = "Hide Answer";
-    }
-  });
-  
+}
 
-  document.getElementById("prev-question").addEventListener("click", () => {
-    currentQuestionIndex--;
-    if (currentQuestionIndex >= 0) {
-      showQuestion();
-    } else {
-      currentQuestionIndex = 0;
-      alert("You've reached the beginning of the questions.");
-    }
-  });
+let currentQuestionIndex = 0;
 
-  
-  
-  document.getElementById("next-question").addEventListener("click", () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < orderedQuestions.length) {
-      showQuestion();
-    } else {
-      currentQuestionIndex = orderedQuestions.length - 1;
-      alert("You've reached the end of the questions.");
-    }
-  });
-  
-  const markForReviewBtn = document.getElementById("mark-for-review");
-  const markedQuestions = new Set();
-  
-  function updateMarkForReviewBtn() {
-    if (markedQuestions.has(currentQuestionIndex)) {
-      markForReviewBtn.textContent = "Unmark for review";
-    } else {
-      markForReviewBtn.textContent = "Mark for review";
-    }
+const questionElement = document.getElementById("question");
+const questionLabelElement = document.getElementById("question-label");
+const answerElement = document.getElementById("answer");
+
+function showQuestion() {
+  questionElement.textContent = orderedQuestions[currentQuestionIndex].question;
+  answerElement.textContent = orderedQuestions[currentQuestionIndex].answer;
+  answerElement.hidden = true;
+  questionLabelElement.textContent = `Question ${currentQuestionIndex + 1} of ${orderedQuestions.length}`;
+  updateMarkForReviewBtn();
+}
+
+document.getElementById("show-answer").addEventListener("click", () => {
+  answerElement.hidden = !answerElement.hidden;
+  if (answerElement.hidden) {
+    document.getElementById("show-answer").textContent = "Show Answer";
+  } else {
+    document.getElementById("show-answer").textContent = "Hide Answer";
   }
-  
-  markForReviewBtn.addEventListener("click", () => {
-    if (markedQuestions.has(currentQuestionIndex)) {
-      markedQuestions.delete(currentQuestionIndex);
-    } else {
-      markedQuestions.add(currentQuestionIndex);
-    }
-    updateMarkForReviewBtn();
-  });
-  
-  showQuestion();
-  
+});
 
+document.getElementById("prev-question").addEventListener("click", () => {
+  currentQuestionIndex--;
+  if (currentQuestionIndex >= 0) {
+    showQuestion();
+  } else {
+    currentQuestionIndex = 0;
+    alert("You've reached the beginning of the questions.");
+  }
+});
+
+document.getElementById("next-question").addEventListener("click", () => {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < orderedQuestions.length) {
+    showQuestion();
+  } else {
+    currentQuestionIndex = orderedQuestions.length - 1;
+    alert("You've reached the end of the questions.");
+  }
+});
+
+const markForReviewBtn = document.getElementById("mark-for-review");
+const markedQuestions = new Set();
+
+function updateMarkForReviewBtn() {
+  if (markedQuestions.has(currentQuestionIndex)) {
+    markForReviewBtn.textContent = "Unmark for review";
+  } else {
+    markForReviewBtn.textContent = "Mark for review";
+  }
+}
+
+markForReviewBtn.addEventListener("click", () => {
+  if (markedQuestions.has(currentQuestionIndex)) {
+    markedQuestions.delete(currentQuestionIndex);
+  } else {
+    markedQuestions.add(currentQuestionIndex);
+  }
+  updateMarkForReviewBtn();
+  saveMarkedQuestions();
+});
+
+// Load marked questions from localStorage.
+loadMarkedQuestions();
+
+showQuestion();
