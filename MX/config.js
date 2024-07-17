0c800c9531a1f6d411ce315a3466ee0f1272190c
marcdamon <mdamon@me.com>
2024-07-17 08:06:45 -0500

    updated API Key

--- MX/config.js
+++ MX/config.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = '816027223942-4rqrbc07n383r8dddfb04rqoojm5480f.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyBUzFa5AscG-sMzbiPxGp0zKDsnbD0ZSGg';
+const API_KEY = 'AIzaSyDbSpFoyOLegym8rouoA7T7FZftbzx83u0';
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 const SPREADSHEET_ID = '1mK2LE_oudXFydiAD_iSuvuL7rPvf6NU8_LlXEVvQQHQ';
